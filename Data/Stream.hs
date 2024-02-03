e6e1187862a770b2832a7ae854c3d3b32d57b52d
Olivia Taliesin <olivia@taliesin.dev>
2024-02-03 13:44:11 +0100

    Semigroup & Monoid

--- Data/Stream.hs
+++ Data/Stream.hs
@@ -9,9 +9,29 @@ module Data.Stream
 
 import GHC.Exts (IsList(..))
 
-data Step a g = Some a !g | Skip !g | Done
+data Step g a = Some a !g | Skip !g | Done
 
-data Stream a = forall g. Stream !(g -> Step a g) !g
+{-# INLINE [0] emptyStep #-}
+emptyStep _ = Done
+
+data Stream a = forall g. Stream !(g -> Step g a) !g
+
+instance Semigroup (Stream a) where
+  {-# INLINE [1] (<>) #-}
+  (Stream nxa sga) <> (Stream nxb sgb) = Stream nx' (Left sga) where
+    {-# INLINE [0] nx' #-}
+    nx' (Left !g)  = case nxa g of
+      Done      -> nx' (Right sgb)
+      Skip g'   -> nx' (Left g')
+      Some x g' -> (Some x (Left g'))
+    nx' (Right !g) = case nxb g of
+      Done      -> Done
+      Skip g'   -> nx' (Right g')
+      Some x g' -> (Some x (Right g'))
+
+instance Monoid (Stream a) where
+  {-# INLINE [0] mempty #-}
+  mempty = Stream emptyStep ()
 
 instance IsList (Stream a) where
   type Item (Stream a) = a
--- Test.hs
+++ Test.hs
@@ -5,11 +5,22 @@ import Test.Tasty.HUnit (testCase, (@?=))
 import Data.Stream
 import GHC.Exts (toList, fromList)
 
-main = defaultMain $ testGroup "Range"
+main = defaultMain $ testGroup "Stream"
   [ toFromList
+  , semigroup
   ]
 
 oneFive :: [Int]
 oneFive = [ 1, 2, 3, 4, 5 ]
 
-toFromList = testCase "To/From Lists" $ (toList . fromList @(Stream Int)) oneFive @?= oneFive
+toStream :: [a] -> Stream a
+toStream = fromList
+
+toFromList = testCase "To/From Lists" $ (toList . toStream) oneFive @?= oneFive
+
+semigroup = testGroup "Semigroup"
+  [ testCase "Two Empty" $ (toList $ (toStream []) <> (toStream @[Int] [])) @?= []
+  , testCase "Left Full" $ (toList $ (toStream oneFive) <> (toStream [])) @?= oneFive
+  , testCase "Rght Full" $ (toList $ (toStream []) <> (toStream oneFive)) @?= oneFive
+  , testCase "Both Full" $ (toList $ (toStream oneFive) <> (toStream oneFive)) @?= oneFive ++ oneFive
+  ]
